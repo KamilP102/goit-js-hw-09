b70237b0ad766ef6b9f250fa07315c8eea9c0e01
KamilP102 <kamilp1319@gmail.com>
2023-02-11 14:25:56 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,14 @@
+const body = document.querySelector("body");
+const startButton = document.querySelector('button[data-start]');
 
+function changeBodyColor() {
+body.style.backgroundColor = getRandomHexColor();
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const myInterval = setInterval(changeBodyColor, 1000);
+
+
+
+startButton.addEventListener("click", myInterval);
