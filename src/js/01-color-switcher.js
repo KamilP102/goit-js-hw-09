e9dab2440335890c2fd348cbf7de79a585da67dc
KamilP102 <kamilp1319@gmail.com>
2023-02-27 15:08:28 +0100

    color switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,14 +1,23 @@
-const body = document.querySelector("body");
+import _ from 'lodash';
+const body = document.querySelector('body');
 const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
 function changeBodyColor() {
-body.style.backgroundColor = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
 }
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const myInterval = setInterval(changeBodyColor, 1000);
+const handelColorChange = startButton.addEventListener('click', () => {
+  changeBodyColor();
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  colorInterval = setInterval(changeBodyColor, 1000);
+});
 
-
-
-startButton.addEventListener("click", myInterval);
+const handelStopButton = stopButton.addEventListener('click', () => {
+  clearInterval(colorInterval);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+});
