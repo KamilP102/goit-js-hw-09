0cc797645635ee2dce4be2afb03c995bcc9ce6af
KamilP102 <110859675+KamilP102@users.noreply.github.com>
2023-02-27 14:09:23 +0000

    Deploying to gh-pages from @ KamilP102/goit-js-hw-09@e9dab2440335890c2fd348cbf7de79a585da67dc 🚀

--- 01-color-switcher.6a6a8c94.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),o=document.querySelector("button[data-start]");const e=setInterval((function(){t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3);o.addEventListener("click",e);
-//# sourceMappingURL=01-color-switcher.6a6a8c94.js.map
--- 01-color-switcher.c12ab758.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),o=document.querySelector("button[data-start]");var e=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3);o.addEventListener("click",e)}();
-//# sourceMappingURL=01-color-switcher.c12ab758.js.map
