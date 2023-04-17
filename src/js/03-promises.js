caf0063a75ee4299d2badf98ff5a200c1a63932a
KamilP102 <kamilp1319@gmail.com>
2023-04-17 21:01:40 +0200

    Update on 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const firstDelay = parseInt(formData.get('delay'), 10);
+  const step = parseInt(formData.get('step'), 10);
+  const amount = parseInt(formData.get('amount'), 10);
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, firstDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
